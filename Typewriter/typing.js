0a6ffcb5295ebadcc7354f29ea13ff1dd6c8173d
vincena02 <adrianvincent1990@gmail.com>
2016-01-04 12:04:39 +0000

    Here are the samples for the template HTML page and also the Javascript functions

---
+++ Typewriter/typing.js
@@ -0,0 +1,10 @@
+$(function(){
+		$("#typed").typed({
+		strings: ["Hello...", "Nice to meet you.", "I hope you enjoy your visit.", "Any questions do not hesitate<br>to ask."],
+		typeSpeed: 50,
+		backDelay: 750,
+		loop: true,
+		loopCount: false,
+		
+	});
+});
